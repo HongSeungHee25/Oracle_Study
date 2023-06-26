c375d2804beabcc36722635ba03d18944ce03be2
홍승희 <seung0476@gmail.com>
2023-06-26 17:19:08 +0900

    0626 SQL 활용 내부평가 예시문제 최종

--- sqlScripts/SQL/0625(1)_SQL 활용 내부평가 연습.sql
+++ sqlScripts/SQL/0625(1)_SQL 활용 내부평가 연습.sql
@@ -179,25 +179,87 @@ AND mem_idx NOT IN (
     WHERE bcode = 'B1101'
 );
 -- 19. "해커스토익" 책을 대여한 회원들 중에서 가장 늦게 반납한 회원의 이름과 대여일자, 반납일자를 조회하시오.
-
-
+SELECT bm.name, br.rent_date, br.return_date
+FROM bookmember bm
+JOIN bookrent br ON bm.mem_idx = br.mem_idx
+JOIN books b ON br.bcode = b.bcode
+WHERE b.title = '해커스토익'
+AND br.return_date = (
+    SELECT MAX(return_date)
+    FROM bookrent
+    WHERE bcode = 'B1101'
+);
 -- 20. "코스모스" 책을 대여한 회원들 중에서 연체 일수가 가장 많은 회원의 이름과 연체 일수를 조회하시오.
+SELECT b.NAME , br.DELAY_DAYS 
+FROM BOOKMEMBER b JOIN BOOKRENT br
+ON b.MEM_IDX = br.MEM_IDX 
+JOIN BOOKS bs ON bs.BCODE = br.BCODE 
+WHERE bs.TITLE = '코스모스'
+AND br.DELAY_DAYS = (
+	SELECT MAX(DELAY_DAYS) 
+	FROM BOOKRENT 
+	WHERE BCODE = 'A1101'
+);
 
+SELECT b.NAME , br.DELAY_DAYS 
+FROM BOOKMEMBER b JOIN BOOKRENT br
+ON b.MEM_IDX = br.MEM_IDX 
+JOIN BOOKS bs ON bs.BCODE = br.BCODE 
+WHERE bs.TITLE = '코스모스'
+AND br.DELAY_DAYS > 0;
 
 -- 21. "푸른사자 와니니" 책을 대여한 회원들 중에서 2023년 6월에 대여한 회원들의 이름과 대여일자를 조회하시오.
+SELECT b.NAME , br.RENT_DATE 
+FROM BOOKMEMBER b JOIN BOOKRENT br
+ON b.MEM_IDX = br.MEM_IDX 
+JOIN BOOKS bs ON bs.BCODE = br.BCODE 
+WHERE bs.TITLE = '푸른사자 와니니' AND to_char(br.RENT_DATE,'yyyy-mm') = '2023-06'
+ORDER BY br.RENT_DATE;
 
+SELECT bm.name, br.rent_date
+FROM bookmember bm
+JOIN bookrent br ON bm.mem_idx = br.mem_idx
+JOIN books b ON br.bcode = b.bcode
+WHERE b.title = '푸른사자 와니니'
+AND br.rent_date BETWEEN DATE '2023-06-01' AND DATE '2023-06-30';
 
 -- 22. "해커스토익" 책을 대여한 회원들 중에서 연락처(tel) 정보를 가지고 있지 않은 회원들의 이름과 이메일 주소를 조회하시오.
-
+SELECT bm.name, bm.email
+FROM bookmember bm
+JOIN bookrent br ON bm.mem_idx = br.mem_idx
+JOIN books b ON br.bcode = b.bcode
+WHERE b.title = '해커스토익'
+AND bm.tel IS NULL;
 
 -- 22. 책을 대여한 회원들 중에서 책을 반납하지 않은 회원들의 이름과 이메일 주소를 조회하시오.
-
+SELECT b.NAME , b.EMAIL 
+FROM BOOKMEMBER b JOIN BOOKRENT br 
+ON b.MEM_IDX = br.MEM_IDX 
+WHERE br.RETURN_DATE IS NULL ;
 
 -- 23. "코스모스" 책을 대여한 회원들 중에서 반납되지 않은 대여 건의 수를 구하시오.
+SELECT count(*) AS "반납 전"
+FROM BOOKRENT br JOIN BOOKS bs
+ON br.BCODE = bs.BCODE 
+WHERE bs.TITLE = '코스모스' AND br.RETURN_DATE IS NULL;
 
-
+SELECT COUNT(*) AS "대여 건의 수"
+FROM bookrent
+WHERE bcode = 'A1101' AND return_date IS NULL;
 -- 24. "해커스토익" 책을 대여한 회원들과 "코스모스" 책을 대여한 회원들의 합집합을 구하시오.
+SELECT b.*
+FROM BOOKMEMBER b JOIN BOOKRENT br
+ON b.MEM_IDX = br.MEM_IDX 
+JOIN BOOKS bs ON bs.BCODE = br.BCODE 
+WHERE bs.TITLE = '해커스토익' AND bs.TITLE = '코스모스';
 
+SELECT mem_idx, bcode
+FROM bookrent
+WHERE bcode = 'B1101'
+UNION ALL
+SELECT mem_idx, bcode
+FROM bookrent
+WHERE bcode = 'A1101';
 
 -- 25. 대여 일자(rent_date)가 가장 오래된 책의 정보를 조회하시오.
 SELECT bs.*
@@ -216,17 +278,54 @@ WHERE pdate = (
 );
 
 -- 26. 각 회원별로 대여한 책의 수를 조회하고, 대여한 책의 수가 가장 많은 회원을 찾으시오.
-
+SELECT b.NAME , COUNT(br.RENT_NO) AS rent_count
+FROM BOOKMEMBER b JOIN BOOKRENT br
+ON b.MEM_IDX = br.MEM_IDX 
+GROUP BY b.NAME 
+HAVING COUNT(br.rent_no) = (
+    SELECT MAX(count_rentals)
+    FROM (
+        SELECT COUNT(rent_no) AS count_rentals
+        FROM bookrent
+        GROUP BY mem_idx
+    )
+);
 
 -- 27. 대여한 책 중에서 중복된 책을 제외한 유일한 책의 목록을 조회하시오.
-
+SELECT b.bcode, b.title, b.writer, b.publisher, b.pdate
+FROM books b
+JOIN (
+    SELECT bcode
+    FROM bookrent
+    GROUP BY bcode
+    HAVING COUNT(*) = 1
+) br ON b.bcode = br.bcode;
 
 -- 28. 대여된 모든 책의 목록과 대여하지 않은 모든 책의 목록을 합쳐서 조회하시오.
-
-
+SELECT b.bcode, b.title, b.writer, b.publisher, b.pdate,br.RENT_DATE 
+FROM books b
+JOIN bookrent br ON b.bcode = br.bcode
+UNION ALL
+SELECT b.bcode, b.title, b.writer, b.publisher, b.pdate, NULL 
+FROM books b
+WHERE b.bcode NOT IN (
+    SELECT bcode
+    FROM bookrent
+);
 -- 29. 아직 반납되지 않은 책 중에서 반납일이 가장 이른 책의 정보를 조회하시오.
-
+SELECT bs.*
+FROM BOOKS bs JOIN BOOKRENT br
+ON bs.BCODE = br.BCODE 
+WHERE br.EXP_DATE = (
+	SELECT MIN(EXP_DATE) AS EXP_DATE 
+	FROM BOOKRENT 
+	WHERE RETURN_DATE IS NULL 
+);
 
 -- 30. 회원별로 대여한 책의 평균 대여 기간을 조회하시오.
+SELECT b.NAME, AVG(TRUNC(br.return_date) - TRUNC(br.rent_date)) AS "평균 대여 기간"
+FROM BOOKMEMBER b
+JOIN BOOKRENT br ON b.MEM_IDX = br.MEM_IDX
+GROUP BY b.NAME;
 
 
